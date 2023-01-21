2036e01b230a19ee4d454c0f95e84d5d895cace8
KanishaL <kanishaliyanage97@gmail.com>
2023-01-21 11:21:31 +0530

    commit box designs

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,10 +1,88 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+
+import '../theme/colors.dart';
+import '../widgets/book_box.dart';
+import '../widgets/genre_box.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  Dio dio = Dio();
+
+  var url = "http://10.0.2.2:3000";
+
+  Future getAllBooks() async {
+    try {
+      var response = await dio.get('$url/seeallbooks');
+      if (response.statusCode == 200) {
+        print(response.data);
+      } else {
+        print("Failed to Fetch Data!");
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    Size size = MediaQuery.of(context).size;
+    var boxWidth = 0.8 * size.width;
+    var boxHeight = 0.35 * size.height;
+    return Scaffold(
+      body: Column(
+        children: [
+          SizedBox(height: 0.08 * size.height),
+          Container(
+            height: 0.1 * size.height,
+            width: double.infinity,
+            margin: EdgeInsets.symmetric(horizontal: 0.08 * size.width),
+            padding: EdgeInsets.only(top: 0.04 * size.width),
+            child: Text(
+              "Hi, Good morning!",
+              //textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Container(
+            height: 0.08 * size.height,
+            width: double.infinity,
+            margin: EdgeInsets.symmetric(horizontal: 0.08 * size.width),
+            padding: EdgeInsets.only(top: 0.04 * size.width),
+            child: Text(
+              "Genres of Books",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+          GenreBox(boxWidth: boxWidth, boxHeight: boxHeight, size: size),
+          SizedBox(height: 0.015 * size.height),
+          Container(
+            height: 0.08 * size.height,
+            width: double.infinity,
+            margin: EdgeInsets.symmetric(horizontal: 0.08 * size.width),
+            padding: EdgeInsets.only(top: 0.04 * size.width),
+            child: Text(
+              "All Books",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+          BookBox(size: size),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: getAllBooks,
+        child: Icon(
+          Icons.arrow_forward_ios,
+        ),
+      ),
+    );
   }
 }
---
+++ lib/widgets/genre_box.dart
@@ -0,0 +1,183 @@
+import 'package:flutter/material.dart';
+
+import '../theme/colors.dart';
+
+class GenreBox extends StatelessWidget {
+  const GenreBox({
+    Key? key,
+    required this.boxWidth,
+    required this.boxHeight,
+    required this.size,
+  }) : super(key: key);
+
+  final double boxWidth;
+  final double boxHeight;
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: boxWidth,
+      height: boxHeight,
+      padding: EdgeInsets.all(
+        0.015 * size.width,
+      ),
+      margin: EdgeInsets.only(
+        bottom: 5,
+        top: 5,
+      ),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(
+            color: shadowColor.withOpacity(0.1),
+            spreadRadius: 1,
+            blurRadius: 1,
+            offset: Offset(1, 1), // changes position of shadow
+          ),
+        ],
+      ),
+      child: Stack(
+        children: [
+          Container(
+            margin: EdgeInsets.all(
+              0.01 * size.width,
+            ),
+            width: double.infinity,
+            height: 0.25 * size.height,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              color: Colors.amber,
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: NetworkImage(
+                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHtVj6yfJohDIk2pWI4vB1_BwSN7tOIhQtfQ&usqp=CAU'),
+              ),
+            ),
+          ),
+          Positioned(
+            top: 0.235 * size.height,
+            right: 0.05 * size.width,
+            child: Container(
+              padding: EdgeInsets.all(
+                0.025 * size.width,
+              ),
+              decoration: BoxDecoration(
+                color: primary,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                    color: shadowColor.withOpacity(0.05),
+                    spreadRadius: 1,
+                    blurRadius: 1,
+                    offset: Offset(0, 0),
+                  ),
+                ],
+              ),
+              child: Text(
+                "200",
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.w500),
+              ),
+            ),
+          ),
+          Positioned(
+            top: 0.265 * size.height,
+            child: Container(
+              width: 0.35 * size.height,
+              padding: EdgeInsets.fromLTRB(
+                0.05 * size.width,
+                0,
+                0.05 * size.width,
+                0,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Harry Pottor",
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                        fontSize: 17,
+                        color: textColor,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  SizedBox(
+                    height: 0.015 * size.height,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.play_circle_outlined,
+                            size: 18,
+                            color: labelColor,
+                          ),
+                          SizedBox(
+                            width: 3,
+                          ),
+                          Text(
+                            "session",
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(color: labelColor, fontSize: 13),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        width: 0.04 * size.width,
+                      ),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.schedule_rounded,
+                            size: 18,
+                            color: labelColor,
+                          ),
+                          SizedBox(
+                            width: 3,
+                          ),
+                          Text(
+                            "duration",
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(color: labelColor, fontSize: 13),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        width: 0.04 * size.width,
+                      ),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.star,
+                            size: 18,
+                            color: yellow,
+                          ),
+                          SizedBox(
+                            width: 3,
+                          ),
+                          Text(
+                            "review",
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(color: labelColor, fontSize: 13),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
