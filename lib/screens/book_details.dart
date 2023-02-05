d138f45db0e0d1a7f5183781f0490dc0bf815d36
KanishaL <kanishaliyanage97@gmail.com>
2023-02-05 11:01:04 +0530

    commit book details page ui

--- lib/screens/book_details.dart
+++ lib/screens/book_details.dart
@@ -10,6 +10,32 @@ class BookDetails extends StatelessWidget {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: PageAppBar("Book Title Here", context),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 0.4 * size.height,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage(
+                    'https://images.ctfassets.net/usf1vwtuqyxm/24YWmI4UcyoMwj7wdKrEcL/374de1941927db12bd844fb197eab11f/English_Harry_Potter_3_Epub_9781781100233.jpg?w=914&q=70&fm=jpg',
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              height: 0.42 * size.height,
+              decoration: BoxDecoration(
+                color: Colors.amber,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(40),
+                  topRight: Radius.circular(40),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
